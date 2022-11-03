ee2cfec65906ba269b54c1909f58d7e39dd1bf65
Victor Feng <victor.feng@surreyschools.ca>
2022-11-03 11:19:25 -0700

    bruh?
    bruh?

---
+++ Unit2Project/Assets/Script/CheckPoint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+
+    public GameObject cuba;
+    Vector3 spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = gameOject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y < -20f)
+        {
+            gameObject.transform.position = spawnPoint;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint")
+        {
+            spawnPoint = cuba.transform.position;
+            Destroy(cuba);
+        }
+    }
+}
