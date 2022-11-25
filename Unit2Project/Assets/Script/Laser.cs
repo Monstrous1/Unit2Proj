5661f9467876be0606be3c320b043d2aa37445bb
Victor Feng <victor.feng@surreyschools.ca>
2022-11-25 14:54:29 -0800

    sfrsdfsb
    bsrfsf

---
+++ Unit2Project/Assets/Script/Laser.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public GameObject tpdeath;
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "Laser")
+        {
+            CharacterController controller = GetComponent<CharacterController>();
+
+            if(controller != null)
+            {
+                controller.enabled = false;
+                controller.transform.position = tpdeath.transform.position;
+                controller.enabled = true;
+            }
+        }
+    }
+
+}
+}
